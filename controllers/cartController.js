b8e562c67122a03622b1e499ecbbb03339e9efbe
--global <--global>
2021-12-20 16:47:41 +0100

    creation of subscribe.ejs and empty cart case

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,11 +3,16 @@ let Cart = require('../models/cartModel');
 let connection = require('../db.js');
 
 exports.showCart = function(req,res){
-    connection.query(`select * from formation WHERE idformation IN (${req.session.user.cart});`, function(error,result) {
-        if (error) console.log(error);
-        console.log(result);
-        res.render('showCart.ejs', {cart: result});
-    });
+    if (req.session.user.cart.length != 0){
+        connection.query(`select * from formation WHERE idformation IN (${req.session.user.cart});`, function(error,result) {
+            if (error) console.log(error);
+            console.log(result);
+            res.render('showCart.ejs', {cart: result});
+        });
+    }
+    else{
+        res.render('showCart.ejs', {cart: []});
+    }
 }
 
 exports.addToCart = function(req,res){
